0a9d1f401a54075ccda4fff5d9f71cf4b8375d37
Asliddin <asliddinshukurjonov@gmail.com>
2022-08-22 09:50:50 +0500

    change login validation

--- routes/auth/schema.js
+++ routes/auth/schema.js
@@ -1,15 +1,15 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().min(4).alphanum().required(),
+  username: Joi.string().min(4).max(15).required(),
+  password: Joi.string().min(4).max(24).alphanum().required(),
 });
 
 const registerSchema = Joi.object({
   firstName: Joi.string().min(0).max(20).required(),
   lastName: Joi.string().min(0).max(30).required(),
-  username: Joi.string().min(0).max(15).required(),
-  password: Joi.string().min(4).alphanum().required(),
+  username: Joi.string().min(4).max(15).required(),
+  password: Joi.string().min(4).max(24).alphanum().required(),
   roleId: Joi.number().required()
 });
 
